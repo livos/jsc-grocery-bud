25d5873a95d549d659b49230c1cfb027fc1821ca
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-09 16:26:28 +0700

    edit item

--- setup/src/List.js
+++ setup/src/List.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items, removeItem }) => {
+const List = ({ items, removeItem, editItem }) => {
   return (
-    <div className="grocery_list">
+    <div className="grocery-list">
       {items.map((item) => {
         const { id, title } = item;
         return (
           <article key={id} className="grocery-item">
             <p className="title">{title}</p>
             <div className="btn-container">
-              <button type="button" className="etit-btn">
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => editItem(id)}
+              >
                 <FaEdit />
               </button>
               <button
